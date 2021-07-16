cc8a5fab51e08a9912b2b040f0ebb60c9053d8fd
La-wii <sentilavale@gmail.com>
2021-07-16 18:18:30 +0200

    add component Courses

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header/>
     <Jumbotron/>
+    <Courses/>
     <Footer/>
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
@@ -12,12 +13,14 @@
 // import HelloWorld from "./components/HelloWorld.vue";
 import Header from "@/components/Header.vue";  
 import Jumbotron from './components/Jumbotron.vue';
+import Courses from './components/Courses.vue';
 import Footer from './components/Footer.vue';
 export default {
   name: "App",
   components: {
     Header,
     Jumbotron,
+    Courses,
     Footer,
   },
 };
