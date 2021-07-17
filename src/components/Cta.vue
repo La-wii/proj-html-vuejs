40d316c180401f2814eb8eb88ed0702ae4bbfea2
La-wii <sentilavale@gmail.com>
2021-07-17 15:27:25 +0200

    component ok

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="cta-section">
+    <div class="cta-section py-5">
         <!-- start container -->
         <div class="container-lg">
             <div class="row">
                 <!-- start col -->
-                <div class="col-3">
+                <div class="col-2">
                     
                 </div>
                 <!-- end col -->
 
                 <!-- start col -->
-                <div class="col-6 text-center">
-                    <h2>Start today for getting <span>Online Certification</span></h2>
-                    <p>You can be your own guiding star with our help!</p>
-                    <button>
+                <div class="col-8 text-center position-relative">
+                    <h4 class="py-3">Start today for getting <span>Online Certification</span></h4>
+                    <h2 class="pb-5">You can be your own guiding star with our help!</h2>
+                    <button class="btn-green">
                         Get started now
                     </button>
+                    <img src="/img/righe.png" alt="righe" class="righe">
+                    <img src="/img/cerchio.png" alt="cerchio" class="cerchio">
                 </div>
                 <!-- end col -->
 
                 <!-- start col -->
-                <div class="col-3">
+                <div class="col-2">
 
                 </div>
                 <!-- end col -->
@@ -41,29 +43,41 @@ export default {
         background-color: #f6f3f5;
             .container-lg{
                 .row{
-                    .col-3{
+                    // .col-2{
 
-                    }
-                    .col-6{
-                        h2{
+                    // }
+                    .col-8{
+                        h4{
                             span{
                                 color: #20AD96;
                             }
                         }
-                        p{
-                            font-size: 20px;
+                        .cerchio{
+                            width: 100px;
+                            position: absolute;
+                            right: -15%;
+                            bottom: 0;
                         }
-                        button{
-                            background-color: #20AD96;
-                            color: white;
-                            padding: 10px 20px;
-                            border:none;
-                            font-size: 18px;
+                        .righe{
+                            width: 180px;
+                            position: absolute;
+                            left: -25%;
+                            bottom: 20%;
                         }
+                        // h2{
+                        //     font-size: 20px;
+                        // }
+                        // button{
+                        //     background-color: #20AD96;
+                        //     color: white;
+                        //     padding: 10px 20px;
+                        //     border:none;
+                        //     font-size: 18px;
+                        // }
                     }
-                    .col-3{
+                    // .col-2{
                         
-                    }
+                    // }
                 }
             }
     }
